1f1115ac679fb6b267c2063566f8f0cf114ad3f5
Israel Yshara Garcia <iyshara@luzdelsur.com.pe>
2019-06-11 16:18:08 -0500

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var model = require("./model.json")
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -10,10 +10,6 @@ app.use(function(req, res, next) {
 
 app.get('/', function (req, res) {
   res.send('Hola Mundo');
-});
-
-app.get('/model.json', function (req, res) {
-  res.send(model);
 });
 
 app.listen(process.env.PORT || 3000, function () {
