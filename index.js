a5af3c4baacdb10848e303c4048775ae76549676
Israel Yshara Garcia <iyshara@luzdelsur.com.pe>
2019-06-12 11:14:42 -0500

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var model;
-const path = "https://example.org/model/weights_manifest.json";
+var path = "https://tensorflowjs-prueba.herokuapp.com/model/model.json";
+var model
 
-window.onload = async () => {
-  await load();
-  console.log('fdgf', model)
-}
+console.log('Inicio')
+load()
 
 async function load() {
-  model = await tf.loadGraphModel(path);
+  model = await tf.loadLayersModel(path)
+  console.log('Modelo: ', model)
+  input = tf.tensor([Array.apply(null, { length: 65 }).map(Number.call, Number)])
+  model.predict(input).print()
 }
