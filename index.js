f747b5776ee887c2838cd3efc985565e0261c46b
Israel Yshara Garcia <iyshara@luzdelsur.com.pe>
2019-06-12 11:55:23 -0500

    sd

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ load()
 async function load() {
   model = await tf.loadLayersModel(path)
   console.log('Modelo: ', model)
-  input = tf.tensor([Array.apply(null, { length: 65 }).map(Number.call, Number)])
-  model.predict(input).print()
+
+  input = [Array(65).fill(0).map((n, i) => i != 0 ? 0 : 1)]
+  console.log(input)
+
+  predict = model.predict(tf.tensor(input))
+  predict.array().then(array => {
+    word = array[0][array.length - 1]
+    console.log(word)
+    idx = word.indexOf(Math.max(...word));
+    console.log(idx)
+  });
 }
